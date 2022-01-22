65dc396198c0002a215e610277d3626966235a2c
Dipen Chauhan <get.dipen@gmail.com>
2022-01-21 22:37:54 -0800

    Initial commit

---
+++ bill.rb
@@ -0,0 +1,27 @@
+require 'date'
+
+class Bill
+  attr_accessor :start_date
+  attr_accessor :end_date
+  attr_accessor :amount
+
+  def initialize(start_date, end_date, amount)
+    
+
+    @start_date = Date.strptime(start_date, "%m-%d-%y")
+    @end_date = Date.strptime(end_date, "%m-%d-%y")
+    @amount = amount
+
+    raise TypeError, "Start date should be a Date" unless @start_date.is_a?(Date)
+    raise TypeError, "End date should be a Date" unless @end_date.is_a?(Date)
+    raise TypeError, "age should be a Float" unless ( @amount.is_a?(Float) || @amount.is_a?(Integer) )
+  end
+
+  def to_s
+    puts "#{@start_date.strftime('%m-%d-%y')} to #{@end_date.strftime('%m-%d-%y')}: $%.2f" % @amount
+  end
+
+end
+
+b1 = Bill.new("1-2-2022", "1-31-2022", 32)
+puts b1.to_s
