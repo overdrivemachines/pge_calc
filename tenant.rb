b6c0d9e953c62f25f1cbcad134048964692217d3
Dipen Chauhan <get.dipen@gmail.com>
2022-01-26 22:38:17 -0800

    added initialize method

--- tenant.rb
+++ tenant.rb
@@ -3,4 +3,15 @@ class Tenant
   attr_accessor :move_in_date
   attr_accessor :move_out_date
 
+
+  def initialize(name, move_in_date, move_out_date)
+    @name = name
+    @move_in_date = Date.strptime(move_in_date, "%m-%d-%y")
+    @move_out_date = Date.strptime(move_out_date, "%m-%d-%y")
+
+    raise TypeError, "Move in date should be a Date" unless @move_in_date.is_a?(Date)
+    raise TypeError, "Move out date should be a Date" unless @move_out_date.is_a?(Date)
+    raise TypeError, "Name should be a String" unless @name.is_a?(String)
+  end
+
 end
